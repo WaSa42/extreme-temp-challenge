cce729a7c86c29113a4daa85af7d82bd0fd2bb85
Wassim DHIF <wassimdhif@gmail.com>
2016-07-25 11:41:16 +0200

    fix

--- server/main.js
+++ server/main.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Attempts } from '../imports/api/attempts.js';
 
-var Api = new Restivus({
+const Api = new Restivus({
     useDefaultAuth: true,
     prettyJson: true
 });
@@ -12,20 +12,15 @@ Api.addCollection(Attempts, {
         post: {
             action: function () {
                 try {
-                    const request = this;
-                    //const request = CryptoJS.AES.decrypt(this, Meteor.Setting.passPhrase);
+                    check(this.bodyParams.user, String);
+                    check(this.bodyParams.temperature, String);
 
-                    console.log(request.bodyParams);
-                    
-                    check(request.bodyParams.user, String);
-                    check(request.bodyParams.temperature, String);
-
-                    if (request.bodyParams.user.length > 30)
+                    if (this.bodyParams.user.length > 30)
                         throw new Meteor.Error('bad-request', 'Username must have 30 char max');
 
-                    request.bodyParams.temperature = parseInt(request.bodyParams.temperature);
+                    this.bodyParams.temperature = parseInt(this.bodyParams.temperature);
                     
-                    Attempts.insert(_.extend(request.bodyParams, {createdAt: new Date()}));
+                    Attempts.insert(_.extend(this.bodyParams, {createdAt: new Date()}));
 
                     return {status: 'success', data: {message: 'Attempt inserted'}};
                 } catch (e) {
